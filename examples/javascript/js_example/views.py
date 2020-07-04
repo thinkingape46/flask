788477cd32df8fa3caec98dfb7bea9dff149416c
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-07-04 18:48:50 +0530

    Update views.py

--- examples/javascript/js_example/views.py
+++ examples/javascript/js_example/views.py
@@ -1,14 +1,16 @@
 from flask import jsonify
 from flask import render_template
 from flask import request
+from flask import Flask
 
-from js_example import app
+# from js_example import app
 
+app = Flask(__name__)
 
 @app.route("/", defaults={"js": "plain"})
 @app.route("/<any(plain, jquery, fetch):js>")
 def index(js):
-    return render_template(f"{js}.html", js=js)
+    return render_template("{js}.html".format(js=js))
 
 
 @app.route("/add", methods=["POST"])
@@ -16,3 +18,6 @@ def add():
     a = request.form.get("a", 0, type=float)
     b = request.form.get("b", 0, type=float)
     return jsonify(result=a + b)
+
+if __name__ == "__main__":
+    app.run()
